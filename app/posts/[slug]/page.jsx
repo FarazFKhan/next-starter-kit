10d5696f0b93008e06198498680d9d6489d873a0
Faraz Khan <farazfkhan@outlook.com>
2023-08-25 11:45:26 +0100

    MDX Docs & Dynamic Rendering

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
+import { getAllPosts, getPostbySlug } from '@/lib/posts';
 
-const LearnNextJsPage = ({ params }) => {
+// This function gets called at build time
+export async function generateStaticParams() {
+   const posts = await getAllPosts()
+
+   // Get the paths we want to pre-render based on posts
+   // Note: We could also fetch this from an external API endpoint
+   return posts.map(post => ({ slug: post.slug }))
+}
+
+
+
+const LearnNextJsPage = async ({ params }) => {
 
    const { slug } = params;
+
+   const { content, frontmatter } = await getPostbySlug(slug)
 
    return (
       <section className='py-24'>
          <div className='container'>
-            <header className="rounded bg-gray-200 p-8">
-               <h1 className='font-serif text-3xl'>{slug}</h1>
+            <header className="rounded bg-gray-200 p-8 mb-10">
+               <h1 className='font-serif text-3xl'>{frontmatter?.title}</h1>
                <p className='text-sm font-light uppercase leading-4 text-gray-500'>
                   Faraz Khan
                </p>
-               {/* Post content */}
             </header>
+
+            {/* {Post Content} */}
+            <main className='prose mt-12'>{content}</main>
          </div>
       </section>
    )
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
+import { getAllPosts } from '@/lib/posts'
 
-const PostsPage = () => {
+const PostsPage = async () => {
+
+   const posts = await getAllPosts()
+   console.log(posts)
+
    return (
-      <>
-         <div className='container'>All Blog Posts</div>
-      </>
+      <section className="py-24">
+         <div className="container">
+            <h1 className="text-3xl font-bold">All blog posts</h1>
+
+            <ul className="mt-12 flex">
+               {posts.map((post) => (
+                  <li key={post.slug} className='mr-8 bg-slate-200 px-4 py-2 rounded-lg'>
+                     <Link href={`/posts/${post.slug}`}>
+                        <h4 className="text-lg font-medium">
+                           {post.frontmatter?.title}
+                        </h4>
+                        <p className='text-sm text-gray-500'>
+                           {post.frontmatter?.author}
+                        </p>
+                     </Link>
+                  </li>
+               ))}
+            </ul>
+         </div>
+      </section>
    )
 }
 
